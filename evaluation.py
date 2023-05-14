dcddcd4aedf14cb2aeeb2a0c20290dea573864bb
Dmytro Borysenkov <dmytroborysenkov@gmail.com>
2023-05-14 13:00:54 +0200

    Added Feature subset selection to data_split + provided CV evaluation function based on class-wise accuracy

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.model_selection import KFold
 
 def calc_accuracy(predictions: np.ndarray, true_labels: np.ndarray, classes=7):
     """
@@ -14,3 +15,21 @@ def calc_accuracy(predictions: np.ndarray, true_labels: np.ndarray, classes=7):
         else:
             acc[f"Class {i}"] = round(len(pred[pred==i])/len(pred)*100,2)
     return acc
+
+def evaluate_cf(classifier, data, labels, k_folds: int):
+    """
+    Perform CV with the calc_accuracy as evaluation criteria
+    """
+    kf = KFold(n_splits=k_folds, shuffle=True, random_state=42)
+    acc = []
+    for train_index, test_index in kf.split(data):
+        X_train, X_test = data.iloc[train_index], data.iloc[test_index]
+        y_train, y_test = labels.iloc[train_index], labels.iloc[test_index]
+        classifier.fit(X_train, y_train)
+        y_predictions = classifier.predict(X_test)
+        temp_acc = calc_accuracy(y_predictions, y_test)
+        print(temp_acc)
+        acc.append(temp_acc)
+    print(classifier)
+    print(f'Estimated accuracy with {k_folds}-CV: {[(key, round(sum([acc[i][key] for i in range(len(acc))])/ len(acc), 2)) for key in acc[0]]}')
+    
